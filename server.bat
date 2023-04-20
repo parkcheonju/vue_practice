70cdc687137fa15750cb60b2e622c82cfcf3fb51
parkcheonju <cjswne@gmail.com>
2023-04-20 17:54:08 +0900

    first commit

---
+++ server.bat
@@ -0,0 +1,4 @@
+cd /d C:\Users\Administrator\Desktop\vue-mango\vue_practice\
+json-server --watch db.json --port 8080
+pause
+.bat
---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="card-body p-2">
+      <div class="d-flex" v-for="i in todos" :key="i.id">
+        <div class="form-check flex-grow-1">
+          <label class="form-check-label" :class="{ todo: i.completed }"> {{ i.subject }}{{ i.id }}<input type="checkbox" class="form-check-input" :value="todos.completed" @change="toggleTodo(i.id)" /></label>
+        </div>
+        <div>
+          <button class="btn btn-danger btn-sm" @click="deleteTodo(i.id)">삭제</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    },
+  },
+  emits: ["toggle-todo", "deleteTodo"],
+  setup(props, { emit }) {
+    const toggleTodo = (index) => {
+      console.log("😀id:", index);
+      emit("toggle-todo", index);
+    };
+    const deleteTodo = (index) => {
+      emit("deleteTodo", index);
+    };
+    return { toggleTodo, deleteTodo };
+  },
+};
+</script>
+
+<style></style>
